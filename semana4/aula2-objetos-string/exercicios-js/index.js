e37d29961d6f4b763e38679c13c7a54a557f8ab5
Luan Bonetto <luanbonetto42@gmail.com>
2019-11-20 18:56:39 -0300

    todos os exercicios do dia 2 feitos

---
+++ semana4/aula2-objetos-string/exercicios-js/index.js
@@ -0,0 +1,60 @@
+// 3)
+
+// function criaRetangulo(lado1, lado2){
+//     let retangulo = {
+//         largura: lado1,
+//         altura: lado2,
+//         perimetro: 2 * (lado1 + lado2),
+//         area: lado1 * lado2
+//     }
+//     console.log(retangulo.largura)
+//     console.log(retangulo.altura)
+//     console.log(retangulo.perimetro)
+//     console.log(retangulo.area)
+// }
+
+// criaRetangulo(10, 20)
+
+
+//-------------------------------------------------------------------------------------
+
+// 4)
+
+// const pessoa = {
+//     nome: "Luan",
+//     idade: 20,
+//     email: "luanbonetto42@gmail.com",
+//     endereco: "Rua U"
+// }
+
+// function anonimizarPessoa(){
+//     const pessoaAnonima = {
+//         nome: pessoa.nome,
+//         idade: pessoa.idade,
+//         email: pessoa.email,
+//         endereco: pessoa.endereco
+//     }
+
+//     pessoaAnonima.nome = "ANÔNIMO"
+
+//     console.log(pessoaAnonima.nome)
+//     console.log(pessoaAnonima.idade)
+//     console.log(pessoaAnonima.email)
+//     console.log(pessoaAnonima.endereco)
+// }
+
+// anonimizarPessoa()
+
+
+//---------------------------------------------------------------------------------------------
+
+// 5)
+
+// const filme = {
+//     titulo: "Angel-A",
+//     ano: 2005,
+//     diretor: "Luc Besson",
+//     atores: ["Rie Rasmussen" , "Jamel Debbouze"]
+// }
+
+// console.log("Venha assistir ao filme " + filme.titulo + "," +"de " + filme.ano + ", dirigido por " + filme.diretor + " e estrelado por " + filme.atores[0] + " e " + filme.atores[1] )
