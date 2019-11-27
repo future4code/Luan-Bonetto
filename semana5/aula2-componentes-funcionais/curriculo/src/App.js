3bc0edae4c0e362dc37f89a720b2533ff20fd92a
Luan Bonetto <luanbonetto42@gmail.com>
2019-11-27 01:10:06 -0300

    Curriculo Feito

---
+++ semana5/aula2-componentes-funcionais/curriculo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './kkkk.png';
+import './App.css';
+import PageSection from './Components/PageSection/PageSection'
+
+function App() {
+  return (
+    <div className="App">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <PageSection></PageSection>
+    </div>
+  );
+}
+
+export default App;
