81d904ed105d1416ef065636d3aecee1004da31e
Luan Bonetto <luanbonetto42@gmail.com>
2019-12-13 08:44:34 -0300

    projeto feito

---
+++ semana7/browsers-e-servidores/app-bored/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BoredAPI from './Components/BoredAPI/index'
+
+function App() {
+  return (
+    <div>
+      <BoredAPI/>
+    </div>
+  );
+}
+
+export default App;
